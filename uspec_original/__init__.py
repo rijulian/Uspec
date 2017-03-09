67787f92d2663c4fec20a63d5f1b1fa7abec077e
rijulian <rijulian@phys.ethz.ch>
2017-03-09 17:23:21 +0100

    Uspec

--- uspec_original/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["generate"]
-__author__ = 'Alexandre Refregier'
-__email__ = 'alexandre.refregier@phys.ethz.ch'
-__version__ = '0.1.0'
-__credits__ = 'ETH Zurich, Institute for Astronomy'
-
-from generate import generate
